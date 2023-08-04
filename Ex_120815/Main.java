bf9e281629d24aadacbadbe61f5f62cf6c825da1
Dojin Park <coah2013@naver.com>
2023-08-04 20:28:25 +0900

    test

---
+++ Ex_120815/Main.java
@@ -0,0 +1,8 @@
+package Ex_120815;
+public class Main {
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        // Test Case
+        test.solution(6);
+    }
+}
---
+++ Ex_120815/Solution.java
@@ -0,0 +1,9 @@
+package Ex_120815;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public void solution(int n) {
+        System.out.println(n);
+    }
+}
