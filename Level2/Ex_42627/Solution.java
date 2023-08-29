7d5248a405d08d30f3b18a285e2f015eb9ca1a6d
Dojin Park <coah2013@naver.com>
2023-08-29 19:46:28 +0900

    230829 풀이 중 Ex_42627 디스크 컨트롤러 다시 풀기 2

--- Level2/Ex_42627/Solution.java
+++ Level2/Ex_42627/Solution.java
@@ -4,61 +4,43 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {2, 6}}));
-//        System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {2, 6}, {1, 4}}));
-
+        System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {2, 6}})); // 9
+        System.out.println(solution(new int[][] {{0, 3}, {10, 1}})); // 2
+        System.out.println(solution(new int[][] {{7, 8}, {3, 5}, {9, 6}})); // 9
+        System.out.println(solution(new int[][] {{1, 4}, {7, 9}, {1000, 3}})); // 5
+        System.out.println(solution(new int[][] {{0, 1}, {2, 2}, {2, 3}})); // 2
     }
 
     public static int solution(int[][] jobs) {
-        int tot_time = 0;
-//         Arrays.sort(jobs, (o1, o2) -> {
-//            return o1[0] - o2[0];
-//        });
-
         PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int tot_time = 0;
         int now = 0;
-        int idx = 0;
-        int target_time = -1;
-        int processing_time = 0;
-        boolean isRunning = false;
 
-        while(idx < jobs.length || !pq.isEmpty() || isRunning) {
-//            System.out.println("now : " + now);
+        for(int i=0; i<jobs.length; i++) {
+            pq.offer(new Pair(jobs[i][0], jobs[i][1]));
+        }
 
-            if(target_time == now) {
-                // 실행중 = false, total 처리시간 합산, 마지막 프로세스 처리시간 초기화
-                isRunning = false;
-                tot_time += processing_time;
-//                processing_time = 0;
-            }
+        while(!pq.isEmpty()) {
+            if(pq.peek().req_time <= now){
+                Pair p = pq.poll();
+                System.out.println(p);
 
-            // offer 조건 (index가 jobs 범위 내인 경우, 현재 시점의 프로세스 요청이 존재하는 경우)
-            while(idx < jobs.length && jobs[idx][0] == now) {
-//                System.out.println("offer");
-                pq.offer(new Pair(jobs[idx][0], jobs[idx][1]));
-//                System.out.println(pq);
-                idx++;
-            }
+                tot_time += now + p.proc_time - p.req_time;
 
-            // poll 조건 (실행중이 아닌 경우)
-            if(!isRunning){
-                if(!pq.isEmpty()){
-                    // 가장 빠른 프로세스 poll, 현재 프로세스 종료 예정시간, 현재 프로세스 처리시간, 실행중 = true
-//                    System.out.println("poll");
-                    Pair proc = pq.poll();
-//                    System.out.println(pq);
-//                    System.out.println("now : " + proc);
-                    target_time = now + proc.proc_time;
-                    processing_time = target_time - proc.req_time;
-                    isRunning = true;
-                }
+                System.out.println("now : " + now);
+                System.out.println("tot_time : " + tot_time);
+
+                now += p.proc_time;
+
+                System.out.println("processed now : " + now);
+                continue;
             }
 
             now++;
         }
 
-//        System.out.println(tot_time);
-        return (int)( tot_time / (double)jobs.length );
+        System.out.println("final tot_time = " + tot_time);
+        return tot_time/jobs.length;
     }
 
     public static class Pair implements Comparable<Pair>{
@@ -71,7 +53,11 @@ public class Solution {
         }
 
         public int compareTo(Pair o) {
-            return this.proc_time - o.proc_time;
+
+            if(this.req_time == o.req_time)
+                return this.proc_time - o.proc_time;
+            else
+                return this.req_time - o.req_time;
         }
 
         @Override
