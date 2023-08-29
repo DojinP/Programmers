bbcea73eeeff67391a0005435d3601d155b75d6b
Dojin Park <coah2013@naver.com>
2023-08-29 20:19:07 +0900

    230829 풀이

--- Level2/Ex_42627/Solution.java
+++ Level2/Ex_42627/Solution.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(solution(new int[][] {{0, 3}, {1, 9}, {2, 6}})); // 9
+        // 반례
         System.out.println(solution(new int[][] {{0, 3}, {10, 1}})); // 2
         System.out.println(solution(new int[][] {{7, 8}, {3, 5}, {9, 6}})); // 9
         System.out.println(solution(new int[][] {{1, 4}, {7, 9}, {1000, 3}})); // 5
@@ -12,34 +13,34 @@ public class Solution {
     }
 
     public static int solution(int[][] jobs) {
+
+        Arrays.sort(jobs, (o1, o2) -> {
+            return o1[0] - o2[0];
+        });
+
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         int tot_time = 0;
         int now = 0;
+        int idx = 0;
 
-        for(int i=0; i<jobs.length; i++) {
-            pq.offer(new Pair(jobs[i][0], jobs[i][1]));
-        }
+        while(!pq.isEmpty() || idx < jobs.length) {
+            while(idx < jobs.length && jobs[idx][0] <= now){
+                pq.offer(new Pair(jobs[idx][0], jobs[idx][1]));
+                idx++;
+            }
 
-        while(!pq.isEmpty()) {
-            if(pq.peek().req_time <= now){
+            if(!pq.isEmpty() && pq.peek().req_time <= now){
+
                 Pair p = pq.poll();
-                System.out.println(p);
-
                 tot_time += now + p.proc_time - p.req_time;
-
-                System.out.println("now : " + now);
-                System.out.println("tot_time : " + tot_time);
-
                 now += p.proc_time;
 
-                System.out.println("processed now : " + now);
                 continue;
             }
 
             now++;
         }
 
-        System.out.println("final tot_time = " + tot_time);
         return tot_time/jobs.length;
     }
 
@@ -53,11 +54,7 @@ public class Solution {
         }
 
         public int compareTo(Pair o) {
-
-            if(this.req_time == o.req_time)
-                return this.proc_time - o.proc_time;
-            else
-                return this.req_time - o.req_time;
+            return this.proc_time - o.proc_time;
         }
 
         @Override
