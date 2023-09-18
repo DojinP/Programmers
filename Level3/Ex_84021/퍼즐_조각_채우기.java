ecd5de16aaffdafe32dd4d6756c49048352c1132
DojinP <coah2013@naver.com>
2023-09-18 17:03:30 +0900

    230915 풀이 중  - 퍼즐 조각 채우기(Lv.3) BFS 풀이  - 6, 7, 8, 10 번 케이스 실패

--- Level3/Ex_84021/퍼즐_조각_채우기.java
+++ Level3/Ex_84021/퍼즐_조각_채우기.java
@@ -12,8 +12,11 @@ public class 퍼즐_조각_채우기 {
 
     public static void main(String[] args) {
 
-        System.out.println(solution(new int[][] {{1,1,0,0,1,0},{0,0,1,0,1,0},{0,1,1,0,0,1},{1,1,0,1,1,1},{1,0,0,0,1,0},{0,1,1,1,0,0}},
-                new int[][] {{1,0,0,1,1,0},{1,0,1,0,1,0},{0,1,1,0,1,1},{0,0,1,0,0,0},{1,1,0,1,1,0},{0,1,0,0,0,0}}));
+        /*System.out.println(solution(new int[][] {{1,1,0,0,1,0},{0,0,1,0,1,0},{0,1,1,0,0,1},{1,1,0,1,1,1},{1,0,0,0,1,0},{0,1,1,1,0,0}},
+                new int[][] {{1,0,0,1,1,0},{1,0,1,0,1,0},{0,1,1,0,1,1},{0,0,1,0,0,0},{1,1,0,1,1,0},{0,1,0,0,0,0}}));*/
+
+        System.out.println(solution(new int[][] {{0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0}, {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1}, {0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1}, {0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0}, {0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0}, {1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1}, {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0}},
+                new int[][] {{1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1}, {1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1}, {1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0}, {0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0}, {1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1}, {1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1}, {0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1}, {1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1}, {1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1}, {1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1}}));
     }
 
     public static int solution(int[][] game_board, int[][] table) {
@@ -26,7 +29,7 @@ public class 퍼즐_조각_채우기 {
                 // 이미 체크된 도형은 2로 표기
                 if (table[y][x] == 1) {
                     table[y][x] = 2; // 방문 처리
-                    bfs(new Point(x, y), table, tableFigureList);
+                    bfs(new Point(x, y), table, tableFigureList, 1);
                 }
             }
         }
@@ -35,54 +38,70 @@ public class 퍼즐_조각_채우기 {
         for(int y=0; y<game_board.length; y++){
             for(int x=0; x<game_board[0].length; x++){
                 // 이미 체크된 도형은 2로 표기
-                if (game_board[y][x] == 1) {
+                if (game_board[y][x] == 0) {
                     game_board[y][x] = 2; // 방문 처리
-                    bfs(new Point(x, y), game_board, gameBoardFigureList);
+                    bfs(new Point(x, y), game_board, gameBoardFigureList, 0);
                 }
             }
         }
 
         // tableFigureList 정렬 (Point[] 사이즈 내림차순)
-//        Collections.sort(tableFigureList, new Comparator<Figure>() {
-//            @Override
-//            public int compare(Figure o1, Figure o2) {
-//                return o2.points.size() - o1.points.size();
-//            }
-//        });
+        /*Collections.sort(tableFigureList, new Comparator<Figure>() {
+            @Override
+            public int compare(Figure o1, Figure o2) {
+                return o2.points.size() - o1.points.size();
+            }
+        });*/
 
         // table 내부 도형 최적화
-        for(Figure figure : tableFigureList){
-            optimizeFigure(figure);
-        }
+        optimizeFigure(tableFigureList);
+        optimizeFigure(gameBoardFigureList);
 
-        for(Figure figure : gameBoardFigureList){
-            optimizeFigure(figure);
-        }
-
-        // figureList 출력
+        // tableFigureList
+        System.out.println("tableFigureList");
         printFigureList(tableFigureList);
-//        printFigureList(gameBoardFigureList);
+        // gameBoardFigureList
+        System.out.println("gameBoardFigureList");
+        printFigureList(gameBoardFigureList);
 
         return checkFit(tableFigureList, gameBoardFigureList);
     }
 
     public static int checkFit(List<Figure> tableFigureList, List<Figure> gameBoardFigureList) {
+        // 일치하는 블럭의 개수
         int count = 0;
 
-        for(int i = 0; i< tableFigureList.size(); i++){
-            Figure figure = tableFigureList.get(i);
-            int sub = Math.min(point.x, point.y);
-            int x = point.x - sub;
-            int y = point.y - sub;
-            for(Point p : figure.points){
-                // 이부분에 들어갈 로직이 궁금해
+        for(Figure gameBoardFigure : gameBoardFigureList){
+            for(Figure tableFigure : tableFigureList) {
+                List<Point> gameBoardPoints = gameBoardFigure.points;
+                List<Point> tablePoints = tableFigure.points;
+
+                if(tableFigure.visited || gameBoardFigure.visited || gameBoardPoints.size() != tablePoints.size()) continue;
+
+                // tableFigure 의 요소들을 회전하면서 gameBoard의 요소들과 비교
+                for(int i=0; i<4; i++){
+                    for(int j=0; j<gameBoardPoints.size(); j++){
+                        Point gp = gameBoardPoints.get(j);
+                        Point tp = tablePoints.get(j);
+                        if(gp.x == tp.x && gp.y == tp.y) {
+                            if(j == gameBoardPoints.size()-1){
+                                gameBoardFigure.visited = true;
+                                tableFigure.visited = true;
+                                count += gameBoardPoints.size();
+                            }
+                        }
+                        else break;
+                    }
+                    if(!gameBoardFigure.visited || !tableFigure.visited) rotateFigure(tableFigure);
+                    else break;
+                }
             }
         }
 
         return count;
     }
 
-    public static void bfs(Point point, int[][] table, List<Figure> figureList){
+    public static void bfs(Point point, int[][] table, List<Figure> figureList, int target){
         Figure figure = new Figure(new ArrayList<>());
         Queue<Point> queue = new LinkedList<>();
 
@@ -98,7 +117,7 @@ public class 퍼즐_조각_채우기 {
 
                 if(nx < 0 || ny < 0 || nx >= table[0].length || ny >= table.length) continue;
 
-                if(table[ny][nx] == 1) {
+                if(table[ny][nx] == target) {
                     table[ny][nx] = 2; // 방문 처리
                     queue.add(new Point(nx, ny));
                 }
@@ -108,50 +127,83 @@ public class 퍼즐_조각_채우기 {
         figureList.add(figure);
     }
 
-    public static void optimizeFigure(Figure figure){
-        int minX = 100, minY = 100;
-        int maxX = -1, maxY = -1;
-        for(Point p : figure.points){
-            if(p.x < minX) minX = p.x;
-            if(p.y < minY) minY = p.y;
-            if(p.x > maxX) maxX = p.x;
-            if(p.y > maxY) maxY = p.y;
-        }
+    public static void optimizeFigure(List<Figure> figureList){
+        for(Figure figure : figureList){
+            int minX = 100, minY = 100;
+            int maxX = -1, maxY = -1;
 
-        figure.sizeX = maxX + 1;
-        figure.sizeY = maxY + 1;
+            for(Point p : figure.points){
+                if(p.x < minX) minX = p.x;
+                if(p.y < minY) minY = p.y;
+                if(p.x > maxX) maxX = p.x;
+                if(p.y > maxY) maxY = p.y;
+            }
 
-        for(Point p : figure.points){
-            p.x -= minX;
-            p.y -= minY;
+            figure.size = Math.max(maxX-minX, maxY-minY);
+//            figure.sizeX = maxX + 1 - minX;
+//            figure.sizeY = maxY + 1 - minY;
+
+            for(Point p : figure.points){
+                p.x -= minX;
+                p.y -= minY;
+            }
+
+            Collections.sort(figure.points, new Comparator<Point>() {
+                @Override
+                public int compare(Point o1, Point o2) {
+                    if(o1.x == o2.x)
+                        return o1.y - o2.y;
+                    else
+                        return o1.x - o2.x;
+                }
+            });
         }
     }
-
+    
+    // 90도 회전
     public static void rotateFigure(Figure figure) {
         for(Point p : figure.points){
+            int temp = p.y;
             p.y = p.x;
-            p.x = figure.sizeY - p.x;
+            p.x = figure.size - temp;
         }
 
+        // 회전 후 정렬
+        Collections.sort(figure.points, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if(o1.x == o2.x)
+                    return o1.y - o2.y;
+                else
+                    return o1.x - o2.x;
+            }
+        });
+
         // figure의 sizeX 와 sizeY 값 switching
-        figure.sizeX += figure.sizeY; // x = 4 + y = 3 ==> x = 7
-        figure.sizeY = figure.sizeX - figure.sizeY; // x = 7 - y = 3 ==> y = 4
-        figure.sizeX -= figure.sizeY; // x = 7 - y = 4 ==> x = 3
+//        figure.sizeX += figure.sizeY; // x = 4 + y = 3 ==> x = 7
+//        figure.sizeY = figure.sizeX - figure.sizeY; // x = 7 - y = 3 ==> y = 4
+//        figure.sizeX -= figure.sizeY; // x = 7 - y = 4 ==> x = 3
     }
 
     public static void printFigureList(List<Figure> figureList){
         for(Figure f : figureList){
-            for(Point p : f.points){
-                System.out.print("[" + p.x + ", " + p.y + "]" + " ");
-            }
-            System.out.println();
+            printFigure(f);
         }
+    }
+
+    public static void printFigure(Figure figure){
+        for(Point p : figure.points){
+            System.out.print("[" + p.y + ", " + p.x + "]" + " ");
+        }
+        System.out.println();
     }
 
     public static class Figure{
         List<Point> points;
-        int sizeX;
-        int sizeY;
+//        int sizeX;
+//        int sizeY;
+        int size;
+        boolean visited;
 
         Figure(List<Point> point){
             this.points = point;
