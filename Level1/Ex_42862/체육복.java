cd0391bf00a3df62db6f766184d9725273a2bff7
Dojin Park <coah2013@naver.com>
2023-09-26 22:28:41 +0900

    230926 풀이  - [탐욕법] 체육복 풀이

--- Level1/Ex_42862/체육복.java
+++ Level1/Ex_42862/체육복.java
@@ -1,6 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42862
 package Level1.Ex_42862;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,71 +11,64 @@ public class 체육복 {
 //        System.out.println(solution(5, new int[] {2, 4}, new int[] {1, 3, 5}));
 //        System.out.println(solution(5, new int[] {2, 4}, new int[] {3}));
 //        System.out.println(solution(3, new int[] {3}, new int[] {1}));
-        System.out.println(solution(3, new int[] {1, 2, 3}, new int[] {2, 3, 4}));
+//        System.out.println(solution(3, new int[] {1, 2, 3}, new int[] {2, 3, 4}));
+        System.out.println(solution(5, new int[] {1, 3}, new int[] {2, 4}));
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
-        n -= lost.length;
+        List<Integer> losts = new ArrayList<Integer>();
+        List<Integer> reserves = new ArrayList<Integer>();
+        List<Integer> students = new ArrayList<Integer>();
 
+        // lost, reserve 배열이 정렬되어있다는 보장이 없다.
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        for (int i = 0; i < lost.length; i++) {
-            if(reserve == null) break;
+        for(int i=1; i<=n; i++) students.add(i);
 
-            System.out.println("lost");
-            System.out.println(Arrays.toString(lost));
+        for(int el : reserve) reserves.add(el);
 
-            System.out.println("reserve");
-            System.out.println(Arrays.toString(reserve));
-
-
-            int target = lost[i];
-
-            int idx = hasTarget(reserve, target);
-            if (idx != -1 && hasTarget(lost, target) == -1) {
-                reserve = deleteReserveElement(reserve, idx);
-                n++;
-                continue;
-            }
-
-            if (target - 1 >= 0) idx = hasTarget(reserve, target - 1);
-            if (idx != -1) {
-                reserve = deleteReserveElement(reserve, idx);
-                n++;
-                continue;
-            }
-
-            if (target + 1 <= n) idx = hasTarget(reserve, target + 1);
-            if (idx != -1) {
-                reserve = deleteReserveElement(reserve, idx);
-                n++;
+        for(int el : lost){
+            // 여벌 옷 있는 학생이 옷을 잃어버린 경우
+            if(reserves.indexOf(el) != -1){
+                reserves.remove(reserves.indexOf(el));
+            }else{
+                losts.add(el);
             }
         }
 
-        return n;
-    }
-
-    public static int[] deleteReserveElement(int[] reserve, int index) {
-        if(reserve.length-1 == 0) return null;
-        int[] temp = new int[reserve.length - 1];
-        int originIdx = 0;
-
-        for (int j = 0; j < temp.length; j++) {
-            if (j == index) {
-                originIdx++;
+        for(int el : losts) {
+            if(el-1 > 0 && el+1 <= n) {
+                if(reserves.indexOf(el-1) != -1) {
+                    reserves.remove(reserves.indexOf(el-1));
+                }else if(reserves.indexOf(el+1) != -1) {
+                    reserves.remove(reserves.indexOf(el+1));
+                }else{
+                    students.remove(students.indexOf(el));
+                }
+            }else {
+                if(el-1 > 0) {
+                    if(reserves.indexOf(el-1) != -1) {
+                        reserves.remove(reserves.indexOf(el-1));
+                    }else{
+                        students.remove(students.indexOf(el));
+                    }
+                }else if(el+1 <= n) {
+                    if(reserves.indexOf(el+1) != -1) {
+                        reserves.remove(reserves.indexOf(el+1));
+                    }else{
+                        students.remove(students.indexOf(el));
+                    }
+                }else {
+                    students.remove(students.indexOf(el));
+                }
             }
-            temp[j] = reserve[originIdx];
-            originIdx++;
         }
 
-        return temp;
-    }
+        System.out.println(reserves);
+        System.out.println(students);
 
-    public static int hasTarget(int[] reserve, int target) {
-        for (int i = 0; i < reserve.length; i++) {
-            if (reserve[i] == target) return i;
-        }
-        return -1;
+        return students.size();
+
     }
 }
