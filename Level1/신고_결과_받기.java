806ace66cc4582fb2eee9764ed5b76312a0b5802
DojinP <coah2013@naver.com>
2023-10-14 18:15:34 +0900

    231014 풀이
    신고 결과 받기 풀었다.

--- Level1/신고_결과_받기.java
+++ Level1/신고_결과_받기.java
@@ -2,55 +2,54 @@ import java.util.*;
 
 public class 신고_결과_받기 {
     public static void main(String[] args) {
+//        System.out.println(Arrays.toString(solution(
+//                new String[] {"muzi", "frodo", "apeach", "neo"},
+//                new String[] {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"},
+//                2
+//                ))); // [2, 1, 1, 0]
+
         System.out.println(Arrays.toString(solution(
-                new String[] {"muzi", "frodo", "apeach", "neo"},
-                new String[] {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"},
-                2
-                ))); // [2, 1, 1, 0]
+                new String[] {"con", "ryan"},
+                new String[] {"ryan con", "ryan con", "ryan con", "ryan con"},
+                3
+        ))); // [0, 0]
     }
 
     public static int[] solution(String[] id_list, String[] report, int k) {
 
-        HashSet<String> reportSet = new HashSet<>();
         boolean[][] reportResult = new boolean[id_list.length][id_list.length];
         int[] reported_count = new int[id_list.length];
         int[] mail_count = new int[id_list.length];
+        StringTokenizer st;
 
-        // Set에 저장하여 중복 제거
+        // report 체크, 신고된 횟수 업데이트
         for(int i=0; i<report.length; i++){
-            reportSet.add(report[i]);
-        }
+            st = new StringTokenizer(report[i]);
+            String reporter = st.nextToken();
+            String villain = st.nextToken();
 
-        // 신고된 횟수 업데이트
-        for(String str : reportSet){
-            String[] temp = str.split(" ");
+            int reporter_idx = 0;
+            int villain_idx = 0;
 
-            for(int i=0; i<id_list.length; i++){
-                if(id_list[i].equals(temp[1])) {
-                    reported_count[i]++;
-                    break;
+            for(int j=0; j<id_list.length; j++) {
+                if(reporter.equals(id_list[j])) {
+                    reporter_idx = j;
+                }else if(villain.equals((id_list[j]))) {
+                    villain_idx = j;
                 }
             }
+
+            if(!reportResult[reporter_idx][villain_idx]) reported_count[villain_idx]++;
+
+            reportResult[reporter_idx][villain_idx] = true;
+
         }
 
         // 신고된 횟수가 k 회 이상인 유저를 신고한 유저의 mail_count 업데이트
         for(int i=0; i<reported_count.length; i++){
             if(reported_count[i] >= k){
-                String target_user = id_list[i];
-
-                for(String str : reportSet){
-                    String[] temp = str.split(" ");
-                    // 신고된 유저가 k 회 이상 신고된 유저인 경우
-                    if(temp[1].equals(target_user)) {
-                        // 신고한 유저를 탐색
-                        for(int j=0; j<id_list.length; j++) {
-                            // 신고한 유저에게 메일 한 통
-                            if(id_list[j].equals(temp[0])) {
-                                mail_count[j]++;
-                                break;
-                            }
-                        }
-                    }
+                for(int j=0; j<id_list.length; j++){
+                    if(reportResult[j][i]) mail_count[j]++;
                 }
             }
         }
